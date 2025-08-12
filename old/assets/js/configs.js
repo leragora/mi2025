1f45827ce1218ed112bbf36fdbd9477e070c68c7
Geraldo Neto <geraldo.linkedit@gmail.com>
2025-08-12 15:12:38 -0300

    atualizado com json e sumário bug pag 157

---
+++ old/assets/js/configs.js
@@ -0,0 +1,352 @@
+export const msoConfigs = {
+  // Pesquisa 
+  item5851: {
+    'graf-01': {
+      containerId: 'item5837',
+      path: './assets/json/graf-01.json'
+    },
+    'graf-faixa': {
+      containerId: 'item5838',
+      path: './assets/json/graf-faixa.json'
+    },
+    'graf-trabalho': {
+      containerId: 'item5839',
+      path: './assets/json/graf-trabalho.json'
+    },
+    'graf-escola': {
+      containerId: 'item5842',
+      path: './assets/json/graf-escola.json'
+    },
+    'graf-religiao': {
+      containerId: 'item5844',
+      path: './assets/json/graf-religiao.json'
+    },
+    'graf-moradia': {
+      containerId: 'item5847',
+      path: './assets/json/graf-moradia.json'
+    },
+    'graf-classe': {
+      containerId: 'item5849',
+      path: './assets/json/graf-classe.json'
+    }
+  },
+  
+  // Categorias
+
+  item26618: {
+    'agua': {
+      containerId: 'item26623',
+      path: './assets/json/pedrazul.json'
+    }
+  }, 
+  
+  item41805: {
+    'agua': {
+      containerId: 'item41810',
+      path: './assets/json/campinho.json'
+    }
+  },
+
+  item12512: {
+    'arroz': {
+      containerId: 'item12518',
+      path: './assets/json/arroz.json'
+    }
+  },
+
+  item11431: {
+    'chocolate': {
+      containerId: 'item11436',
+      path: './assets/json/chocolate.json'
+    },
+    'industria-capixaba': {
+      containerId: 'item11447',
+      path: './assets/json/industria.json'
+    }
+  },
+
+  item17481: {
+    'trigo': {
+      containerId: 'item17486',
+      path: './assets/json/trigo.json'
+    },
+    'bolo': {
+      containerId: 'item17496',
+      path: './assets/json/bolo.json'
+    }
+  },  
+
+  item41220: {
+    'linguica': {
+      containerId: 'item41225',
+      path: './assets/json/linguica.json'
+    }
+  },
+
+  item14903: {
+    'macarrao': {
+      containerId: 'item14908',
+      path: './assets/json/macarrao.json'
+    }
+  },
+
+  item19958: {
+    'padaria': {
+      containerId: 'item19963',
+      path: './assets/json/monza.json'
+    }
+  },
+
+  item38891: {
+    'padaria': {
+      containerId: 'item38896',
+      path: './assets/json/libano.json'
+    }
+  },
+
+  item38240: {
+    'sal': {
+      containerId: 'item38245',
+      path: './assets/json/sal.json'
+    }
+  },
+
+  item28464: {
+    'supermercado': {
+      containerId: 'item28469',
+      path: './assets/json/supermercado.json'
+    }
+  },
+
+  item43532: {
+    'moveis': {
+      containerId: 'item43539',
+      path: './assets/json/moveisfabrica.json'
+    }
+  },
+
+  item35957: {
+    'informatica': {
+      containerId: 'item35962',
+      path: './assets/json/informatica.json'
+    }
+  },
+
+  item13697: {
+    'loja-roupa': {
+      containerId: 'item13702',
+      path: './assets/json/loja-roupa.json'
+    }
+  },
+
+  item36435: {
+    'utilidades': {
+      containerId: 'item36440',
+      path: './assets/json/utilidades.json'
+    }
+  }, 
+
+  item16138: {
+    'ead': {
+      containerId: 'item16143',
+      path: './assets/json/ead.json'
+    },
+      'superior': {
+      containerId: 'item16155',
+      path: './assets/json/superior.json'
+    },
+      'posgraduacao': {
+      containerId: 'item16167',
+      path: './assets/json/posgraduacao.json'
+    }
+  }, 
+
+  item23444: {
+    'ensino-fundamental': {
+      containerId: 'item23449',
+      path: './assets/json/fundamental.json'
+    },
+      'ensino-infantil': {
+      containerId: 'item23459',
+      path: './assets/json/infantil.json'
+    }
+  }, 
+
+  item21688: {
+    'ensino-tecnico': {
+      containerId: 'item21693',
+      path: './assets/json/tecnico.json'
+    }
+  },  
+
+  item24089: {
+    'cooperativa': {
+      containerId: 'item24094',
+      path: './assets/json/cooperativa.json'
+    }
+  },  
+
+  item26081: {
+    'grande-empresa': {
+      containerId: 'item26086',
+      path: './assets/json/empresa.json'
+    }
+  }, 
+
+  item29632: {
+    'ferragens': {
+      containerId: 'item29637',
+      path: './assets/json/ferragens.json'
+    }
+  }, 
+
+  item14279: {
+    'construtora': {
+      containerId: 'item14285',
+      path: './assets/json/construtora.json'
+    },
+      'luxo': {
+      containerId: 'item14294',
+      path: './assets/json/luxo.json'
+    }
+  },   
+
+  item15447: {
+    'construtora': {
+      containerId: 'item15452',
+      path: './assets/json/mrv.json'
+    }
+  },   
+
+  item42984: {
+    'loteamento': {
+      containerId: 'item42989',
+      path: './assets/json/loteamento.json'
+    },
+      'imobiliaria': {
+      containerId: 'item42998',
+      path: './assets/json/imobiliaria.json'
+    }
+  }, 
+
+  item27745: {
+    'acabamentos': {
+      containerId: 'item27750',
+      path: './assets/json/acabamentos.json'
+    }
+  },  
+
+  item16856: {
+    'tintas': {
+      containerId: 'item16862',
+      path: './assets/json/tintas.json'
+    }
+  }, 
+
+  item44256: {
+    'cidade': {
+      containerId: 'item44261',
+      path: './assets/json/cidade.json'
+    }
+  }, 
+
+  item18716: {
+    'lazer': {
+      containerId: 'item18721',
+      path: './assets/json/sesc.json'
+    }
+  },  
+
+  item11992: {
+    'entreterimento': {
+      containerId: 'item11997',
+      path: './assets/json/china.json'
+    }
+  },  
+
+  
+  item37583: {
+    'parque': {
+      containerId: 'item37588',
+      path: './assets/json/aquatico.json'
+    }
+  },   
+
+  item19391: {
+    'shopping': {
+      containerId: 'item19396',
+      path: './assets/json/shopping.json'
+    }
+  },    
+
+  item18061: {
+    'manipula': {
+      containerId: 'item18066',
+      path: './assets/json/manipula.json'
+    }
+  },  
+
+  item20485: {
+    'hospital': {
+      containerId: 'item20490',
+      path: './assets/json/hospital.json'
+    }
+  }, 
+
+  item37001: {
+    'maternidade': {
+      containerId: 'item37006',
+      path: './assets/json/maternidade.json'
+    }
+  },   
+  
+  item25020: {
+    'saude': {
+      containerId: 'item25025',
+      path: './assets/json/planosaude.json'
+    }
+  },   
+  
+  item10416: {
+    'grafica': {
+      containerId: 'item10421',
+      path: './assets/json/grafica.json'
+    }
+  },  
+
+  item42342: {
+    'internet': {
+      containerId: 'item42347',
+      path: './assets/json/internet.json'
+    }
+  },  
+
+  item9867: {
+    'motocicleta': {
+      containerId: 'item9872',
+      path: './assets/json/motocicletas.json'
+    }
+  },
+
+  item13110: {
+    'veiculos': {
+      containerId: 'item13115',
+      path: './assets/json/veiculos.json'
+    }
+  },
+
+  item27172: {
+    'protecao-veicular': {
+      containerId: 'item27177',
+      path: './assets/json/protecaoveiculo.json'
+    }
+  },   
+
+  item6517: {
+    'passageiros': {
+      containerId: 'item6471',
+      path: './assets/json/passageiros.json'
+    }
+  }, 
+
+}
