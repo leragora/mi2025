0a92711a0efe199ceb2ae22c7985a0820d06ed8a
Geraldo Neto <geraldo.linkedit@gmail.com>
2025-08-12 18:26:44 -0300

    html divide final

--- old/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-// main.js
-
-import { setupMSO } from './observerSetup.js';
-import { msoConfigs } from './configs.js'; 
-
-document.addEventListener('DOMContentLoaded', () => {
-  // Para cada ID de MSO nas chaves de msoConfigs
-  Object.keys(msoConfigs).forEach(setupMSO);
-});
